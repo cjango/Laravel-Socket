9521ccad3b00fe384af0ab8f4a6b38ffcb724d04
小陈叔叔 <chenjxlg@163.com>
2018-06-19 17:16:29 +0800

    update

--- src/Commands.php
+++ src/Commands.php
@@ -17,12 +17,17 @@ class Commands extends Command
 
     public function handle()
     {
+        $this->checkEventFile();
+
+        $this->info('Welcome use Workerman server');
+
         global $argv;
 
         $action = $this->argument('action');
 
         if (!in_array($action, ['start', 'stop', 'restart', 'reload', 'status', 'connections'])) {
-            return 'Error';
+            echo "Error";
+            return;
         }
         $argv[1] = $action;
         $argv[2] = $this->option('d') ? '-d' : '';
@@ -77,4 +82,29 @@ class Commands extends Command
     {
         new Register('text://0.0.0.0:1236');
     }
+
+    private function checkEventFile()
+    {
+        if (!is_dir(app_path('Workerman'))) {
+            $this->warn('Workerman Path Notexist');
+            $this->laravel['files']->makeDirectory(app_path('Workerman'), 0755, true, true);
+            $this->info('Make Workerman Path was created.');
+        }
+
+        $eventFile = app_path('Workerman/Events.php');
+        if (file_exists($eventFile)) {
+            $this->info('Workerman EventFile already exists');
+        } else {
+            $contents = $this->getStub('event');
+            $this->warn('Workerman EventFile Notexist');
+            $this->laravel['files']->put($eventFile, $contents);
+            $this->info('Make Workerman EventFile was created.');
+        }
+    }
+
+    private function getStub($name)
+    {
+        return $this->laravel['files']->get(__DIR__ . "/stubs/$name.stub");
+    }
+
 }
