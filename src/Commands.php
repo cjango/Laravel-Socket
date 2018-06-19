499bd924705a245cc5aa679b7ec8a747604ae3ba
小陈叔叔 <chenjxlg@163.com>
2018-06-19 16:14:51 +0800

    0.0.1

---
+++ src/Commands.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace RuLong\Panel;
+
+use Illuminate\Console\Command;
+
+class Commands extends Command
+{
+    protected $signature = 'workman {action} {--d}';
+
+    protected $description = 'Operate a Workerman server.';
+
+    public function handle()
+    {
+        global $argv;
+        $action = $this->argument('action');
+
+        $argv[1] = $action;
+        $argv[2] = $this->option('d') ? '-d' : '';
+
+        switch ($arg) {
+            case 'start':
+                $this->start();
+                break;
+            case 'stop':
+                break;
+            case 'restart':
+                break;
+            case 'reload':
+                break;
+            case 'status':
+                break;
+            case 'connections':
+                break;
+        }
+    }
+
+    private function start()
+    {
+        $this->startGateWay();
+        $this->startBusinessWorker();
+        $this->startRegister();
+        Worker::runAll();
+    }
+
+    private function startBusinessWorker()
+    {
+        $worker                  = new BusinessWorker();
+        $worker->name            = 'BusinessWorker';
+        $worker->count           = 1;
+        $worker->registerAddress = '127.0.0.1:1236';
+        $worker->eventHandler    = Events::class;
+    }
+
+    private function startGateWay()
+    {
+        $context = [
+            'ssl' => [
+                'local_cert'  => '/yourSSLcert.pem',
+                'local_pk'    => '/yourSSLcert.key',
+                'verify_peer' => false,
+            ],
+        ];
+
+        // 开启wss模式
+        // $gateway = new Gateway("websocket://0.0.0.0:2346", $context);
+        // $gateway->transport            = 'ssl';
+
+        $gateway = new Gateway("websocket://0.0.0.0:2346");
+
+        $gateway->name                 = 'Gateway';
+        $gateway->count                = 1;
+        $gateway->lanIp                = '127.0.0.1';
+        $gateway->startPort            = 2300;
+        $gateway->pingInterval         = 30;
+        $gateway->pingNotResponseLimit = 0;
+        $gateway->pingData             = '{"type":"@heart@"}';
+        $gateway->registerAddress      = '127.0.0.1:1236';
+    }
+
+    private function startRegister()
+    {
+        new Register('text://0.0.0.0:1236');
+    }
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RuLong\Panel;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+
+    protected $commands = [
+        Commands::class,
+    ];
+
+    public function boot()
+    {
+        $this->commands($this->commands);
+
+        if ($this->app->runningInConsole()) {
+
+        }
+    }
+
+    public function register()
+    {
+
+    }
+}
